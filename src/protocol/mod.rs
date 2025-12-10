9aa210816f09b7413c4613b8d96f5604da5425e2
Josh Caponigro <joshcaponigro@protonmail.com>
2025-12-09 23:39:36 -0600

    add auth protocol types

---
+++ src/protocol/mod.rs
@@ -0,0 +1,7 @@
+pub mod types;
+
+pub use types::{
+      SOCKS_VERSION,
+      RSV,
+      AuthMethodCode
+  };
---
+++ src/protocol/types.rs
@@ -0,0 +1,30 @@
+/// SOCKS5 protocol version
+pub const SOCKS_VERSION: u8 = 0x05;
+
+/// Reserved byte
+pub const RSV: u8 = 0x00;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(u8)]
+pub enum AuthMethodCode {
+    NoAuth = 0x00,
+    Gssapi = 0x01,
+    UsernamePassword = 0x02,
+    // 0x03-0x7F: IANA assigned
+    // 0x80-0xFE: Reserved for private methods
+    NoAcceptable = 0xFF,
+}
+
+impl TryFrom<u8> for AuthMethodCode {
+    type Error = u8;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x00 => Ok(Self::NoAuth),
+            0x01 => Ok(Self::Gssapi),
+            0x02 => Ok(Self::UsernamePassword),
+            0xFF => Ok(Self::NoAcceptable),
+            other => Err(other),
+        }
+    }
+}
